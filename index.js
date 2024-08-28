56b1a2621abc8fe53520964c4ce8df2cd5737293
WYC-E OnGitHub <easonwyce@163.com>
2024-08-28 23:32:03 +0800

    fix(github-pages): 修复404页面并改进Markdown加载
    - 新增一个404.html文件，以自定义GitHub Pages上的404页面，帮助用户更好地导航回首页。
    - 改进index.html中的Markdown加载机制，添加一个按钮来显式触发Markdown内容的加载，解决可能的加载缓慢或失败问题，并优化用户交互。
    - 调整index.js以支持新的Markdown加载流程，隐藏加载提示，直到用户点击按钮触发加载，同时确保Markdown加载成功后才显示内容。

--- index.js
+++ index.js
@@ -12,30 +12,41 @@ setInterval(updateClock, 1000);
 const musicid = '2124731026';
 document.getElementById('music').src = `//music.163.com/outchain/player?type=2&id=${musicid}&auto=1&height=66`;
 
-    // 加载Markdown文件的函数
-    function loadMarkdown(url, elementId) {
-        fetch(url)
-            .then(response => response.text())
-            .then(markdownText => {
-                // 使用marked.js解析Markdown
-                const htmlContent = marked.parse(markdownText);
-                // 将解析后的HTML插入到页面中指定的元素
-                document.getElementById(elementId).innerHTML = htmlContent;
-            })
-            .catch(error => {
-                console.error('加载Markdown文件时出错：', error);
-                document.getElementById(elementId).innerText = '加载Markdown内容失败！';
-            });
-    }
 
+// -------------------------------------------markdown加载--------------------------------------------
+ // 加载Markdown文件的函数
+function loadMarkdown(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(markdownText => {
+           // 使用marked.js解析Markdown
+            const htmlContent = marked.parse(markdownText);
+            // 将解析后的HTML插入到页面中指定的元素
+            document.getElementById(elementId).innerHTML = htmlContent;
+        })
+        .catch(error => {
+            console.error('加载Markdown文件时出错：', error);
+            document.getElementById(elementId).innerText = '加载Markdown内容失败！';
+        });
+}
+
+document.getElementById('markdown!').style.display = 'none';
+
+document.getElementById('inputmarkdown').addEventListener('click', () => {
     // 调用函数，加载并显示Markdown内容
     loadMarkdown('https://raw.githubusercontent.com/WYC-E/WYC-E/main/README.md', 'markdownContent');
+    document.getElementById('inputmarkdown').style.display = 'none';
+    document.getElementById('markdown!').style.display = 'block';
+});
+// -------------------------------------------markdown加载--------------------------------------------
 
-    window.onload = function() {  
-        var path = window.location.search.replace('?img?', '');
-        if (path === '') {
-            document.body.style.background.url = 'index/footage/backgroundimg.jpg';
-        } else {
-            document.body.style.background.url = path;
-        }
-    };
+
+
+window.onload = function() {  
+    var path = window.location.search.replace('?img?', '');
+    if (path === '') {
+        document.body.style.background.url = 'index/footage/backgroundimg.jpg';
+    } else {
+        document.body.style.background.url = path;
+    }
+};
