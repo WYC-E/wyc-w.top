a4c6b48283ed86726101ff08861b2a084c6bfbbf
WYC-E OnGitHub <easonwyce@163.com>
2024-12-11 21:11:15 +0800

    refactor: 优化网站代码结构和样式
    - 调整了 HTML 结构，使代码更加清晰
    - 优化了 CSS 样式，删除了冗余的注释
    - 简化了 JavaScript 代码，提高了可读性
    - 统一了变量命名风格
    - 调整了部分布局，使页面更加美观

--- index.js
+++ index.js
@@ -6,22 +6,17 @@ function updateClock() {
 
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 }
-// 每秒更新一次时间
+
 setInterval(updateClock, 1000);
 
-const musicid = '2624184715';
-document.getElementById('music').src = `//music.163.com/outchain/player?type=2&id=${musicid}&auto=1&height=66`;
+const musicId = '2624184715';
+document.getElementById('music').src = `//music.163.com/outchain/player?type=2&id=${musicId}&auto=1&height=66`;
 
-
-// -------------------------------------------markdown加载--------------------------------------------
- // 加载Markdown文件的函数
 function loadMarkdown(url, elementId) {
     fetch(url)
         .then(response => response.text())
         .then(markdownText => {
-           // 使用marked.js解析Markdown
             const htmlContent = marked.parse(markdownText);
-            // 将解析后的HTML插入到页面中指定的元素
             document.getElementById(elementId).innerHTML = htmlContent;
         })
         .catch(error => {
@@ -33,34 +28,21 @@ function loadMarkdown(url, elementId) {
 document.getElementById('markdown!').style.display = 'none';
 
 document.getElementById('inputmarkdown').addEventListener('click', () => {
-    // 调用函数，加载并显示Markdown内容
     loadMarkdown('https://raw.githubusercontent.com/WYC-E/WYC-E/main/README.md', 'markdownContent');
     loadMarkdown('./README.md', 'markdownContent_web');
     document.getElementById('inputmarkdown').style.display = 'none';
     document.getElementById('markdown!').style.display = 'block';
 });
-// -------------------------------------------markdown加载--------------------------------------------
 
-
-
-window.onload = function() {  
-    var path = window.location.search.replace('?img?', '');
-    if (path === '') {
-        document.body.style.background.url = 'index/footage/backgroundimg.jpg';
-    } else {
-        document.body.style.background.url = path;
-    }
+window.onload = function() {
+    const path = window.location.search.replace('?img?', '');
+    document.body.style.backgroundImage = path ? `url(${path})` : 'url(index/footage/backgroundimg.jpg)';
 };
-
 
 function isMobile() {
     const userAgentInfo = navigator.userAgent;
     const mobileAgents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
-     const mobileFlag = mobileAgents.some((mobileAgent) => {
-        return userAgentInfo.indexOf(mobileAgent) > 0;
-    });
-
-    return mobileFlag;
+    return mobileAgents.some(agent => userAgentInfo.indexOf(agent) > 0);
 }
 
 console.log(isMobile() ? '移动端' : 'PC端');
